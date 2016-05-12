6559f1edfa11c58f43a2c2e4cd17c2638c43641d
Sanitas <Sanitas>
2016-05-12 10:31:53 +0200

    segundo commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -20,7 +20,7 @@
 
 		    public static void main(String[] args) {
 		            // Prints "Hello, World" to the terminal window.
-			            System.out.println("Hello, Santi");
+			            System.out.println("Hello, Raul");
 				        }
 
 					}
